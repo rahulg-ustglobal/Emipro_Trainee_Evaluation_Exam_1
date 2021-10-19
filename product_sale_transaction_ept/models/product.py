2cbeee43cdcbd8cf5148775a940196297ed50c89
Rahul Ghanghave <rahulg@emiprotechnologies.com>
2021-10-19 10:39:58 +0530

    [ADD] Emipro Trainee Evaluation || Exam - 1 Modified_19/10/2021_(Added Comments)

--- product_sale_transaction_ept/models/product.py
+++ product_sale_transaction_ept/models/product.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     _name = "product.ept"
     _description = "Product Model"
 
-    name = fields.Char(string='product Name', help="Takes Product Name", required=True)
+    name = fields.Char(string='Product Name', help="Takes Product Name", required=True)
     price = fields.Float(string="Product Price", help='Takes Product Price', digits=(6, 2), required=True)
     product_sku = fields.Char(string='Product Sku', help='Takes Product Sku', required=True)
     uom_id = fields.Many2one(comodel_name='product.uom.ept',string="Uom ID")
